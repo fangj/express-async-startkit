f23a7bd7d544a8e0e8cae88378bbeb2183e2893a
Fang Jian <fangjianmail@126.com>
2018-01-22 16:06:45 +0800

    disable schedule by default

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-require('./schedule'); //run schedule task
+// require('./schedule'); //run schedule task
 
 var app = express();
 
